7b633e322c589776ede65d48cfba9e69a82d0948
MazenAdel1 <ma134652134652@gmail.com>
2023-11-24 02:03:51 +0200

    try to fix routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./rtk/store.jsx";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/recipe-app">
+    <BrowserRouter basename="/recipe-app">
       <Provider store={store}>
         <App />
       </Provider>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
 );
